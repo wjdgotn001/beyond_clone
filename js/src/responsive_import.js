d4d5ecf6b26c1cfa77b551bc66811d61025fc062
wjdgotn001 <wjdgotn001@gmail.com>
2021-01-17 22:53:35 +0900

    seller_layout

--- js/src/responsive_import.js
+++ js/src/responsive_import.js
@@ -5,20 +5,23 @@
   //디바이스 타입
   var deviceType = ['mobile', 'tablet', 'pc', 'pcfull'];
 
-  var url = './common/';
   var pcUrl = './pc/';
   var headBox = $('#headBox');
+  var viewBox = $('#viewBox');
+  var sellerBox = $('#sellerBox');
 
   //import file 
   
 
   //file separation
   if(nowW === deviceType[0]){
-    headBox.load( url + 'headBox.html' );
+
   }else if(nowW === deviceType[1]){
-    headBox.load( url + 'headBox.html' );
+
   }else{
     headBox.load( pcUrl + 'pc_headBox.html' )
+    viewBox.load( pcUrl + 'pc_viewBox.html' )
+    sellerBox.load( pcUrl + 'pc_seller.html' )
   }
 
 

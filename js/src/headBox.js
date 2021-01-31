6c9d94df4eeee09d8d2095dca054e5d9a6b1dbcb
wjdgotn001 <wjdgotn001@gmail.com>
2021-01-31 21:52:42 +0900

    gnb_accordion

--- js/src/headBox.js
+++ js/src/headBox.js
@@ -34,9 +34,10 @@
   });
 
 
-var categoryCon = [
+var arr = [
   { 'title':{'name':'스킨케어','link':'beyond.html','target':'_blank'},
-    'subContent':[
+    'sub':[
+    [
      {'subC':'스킨/토너', 'subLink':'beyond.html'},
      {'subC':'에멀전/로션', 'subLink':'beyond.html'},
      {'subC':'에센스/세럼', 'subLink':'beyond.html'},
@@ -47,43 +48,10 @@ var categoryCon = [
      {'subC':'베이비/주니어', 'subLink':'beyond.html'},
      {'subC':'클렌저', 'subLink':'beyond.html'},
      {'subC':'마스크팩/스크럽', 'subLink':'beyond.html'},
-     {'subC':'기초세트', 'subLink':'beyond.html'}]
-   },
+     {'subC':'기초세트', 'subLink':'beyond.html'}
+    ],
 
-   { 'title':{'name':'바디','link':'beyond.html','target':'_blank'},
-     'subContent':[
-     {'subC':'바디클렌저', 'subLink':'beyond.html'},
-     {'subC':'바디에멀전/크림', 'subLink':'beyond.html'},
-     {'subC':'바디오일/미스트', 'subLink':'beyond.html'},
-     {'subC':'핸드/풋/스크럽', 'subLink':'beyond.html'},
-     {'subC':'키즈', 'subLink':'beyond.html'},
-     {'subC':'바디세트', 'subLink':'beyond.html'}]
-    },
-
-    { 'title':{'name':'헤어','link':'beyond.html','target':'_blank'},
-      'subContent':[
-     {'subC':'샴푸/린스', 'subLink':'beyond.html'},
-     {'subC':'에센스/트리트먼트/스케일링', 'subLink':'beyond.html'},
-     {'subC':'스타일링', 'subLink':'beyond.html'}]
-    },
-
-    { 'title':{'name':'메이크업','link':'beyond.html','target':'_blank'},
-      'subContent':[
-     {'subC':'페이스', 'subLink':'beyond.html'},
-     {'subC':'아이', 'subLink':'beyond.html'},
-     {'subC':'립', 'subLink':'beyond.html'},
-     {'subC':'네일', 'subLink':'beyond.html'}]
-    },
-
-    { 'title':{'name':'온라인 전용','link':'beyond.html','target':'_blank'},
-      'subContent':[
-     {'subC':'온라인 전용상품', 'subLink':'beyond.html'}]
-    }
-];
-
-var lineCon = [
-  {'title':{'name':'스킨케어','link':'beyond.html','target':'_blank'},
-  'subContent':[
+    [
     {'subL':'트루 에코', 'subLink':'beyond.html'},
     {'subL':'타임리스', 'subLink':'beyond.html'},
     {'subL':'피토 아쿠아', 'subLink':'beyond.html'},
@@ -97,36 +65,81 @@ var lineCon = [
     {'subL':'키즈에코', 'subLink':'beyond.html'},
     {'subL':'베이비 퓨어', 'subLink':'beyond.html'},
     {'subL':'에코 데일리 디펜스', 'subLink':'beyond.html'},
-    {'subL':'미라클 포레스트', 'subLink':'beyond.html'} ]
-  },
+    {'subL':'미라클 포레스트', 'subLink':'beyond.html'} 
+    ]
+    ]},
 
   { 'title':{'name':'바디','link':'beyond.html','target':'_blank'},
-  'subContent':[
-  {'subL':'딥 모이스처', 'subLink':'beyond.html'},
-  {'subL':'딥 모이스처 시그니처', 'subLink':'beyond.html'},
-  {'subL':'토탈 리커버리', 'subLink':'beyond.html'},
-  {'subL':'바디 리프팅', 'subLink':'beyond.html'},
-  {'subL':'바디 디펜스', 'subLink':'beyond.html'},
-  {'subL':'리바이탈', 'subLink':'beyond.html'},
-  {'subL':'워터풀', 'subLink':'beyond.html'},
-  {'subL':'키즈에코', 'subLink':'beyond.html'} ]
- },
+     'sub':[
+    [
+     {'subC':'바디클렌저', 'subLink':'beyond.html'},
+     {'subC':'바디에멀전/크림', 'subLink':'beyond.html'},
+     {'subC':'바디오일/미스트', 'subLink':'beyond.html'},
+     {'subC':'핸드/풋/스크럽', 'subLink':'beyond.html'},
+     {'subC':'키즈', 'subLink':'beyond.html'},
+     {'subC':'바디세트', 'subLink':'beyond.html'}
+    ],
 
-  {'title':{'name':'헤어','link':'beyond.html','target':'_blank'},
-     'subContent':[
-     {'subL':'프로페셔널 디펜스', 'subLink':'beyond.html'},
-     {'subL':'스캘프 클리닉', 'subLink':'beyond.html'},
-     {'subL':'딥 클린', 'subLink':'beyond.html'},
-     {'subL':'데미지 리페어', 'subLink':'beyond.html'},
-     {'subL':'키즈에코', 'subLink':'beyond.html'},
-     {'subL':'아르간 테라피', 'subLink':'beyond.html'},
-     {'subL':'스페셜', 'subLink':'beyond.html'},
-     {'subL':'스트레스 릴리프', 'subLink':'beyond.html'},
-     {'subL':'딥모이스처', 'subLink':'beyond.html'}]
-    },
+    [
+    {'subL':'딥 모이스처', 'subLink':'beyond.html'},
+    {'subL':'딥 모이스처 시그니처', 'subLink':'beyond.html'},
+    {'subL':'토탈 리커버리', 'subLink':'beyond.html'},
+    {'subL':'바디 리프팅', 'subLink':'beyond.html'},
+    {'subL':'바디 디펜스', 'subLink':'beyond.html'},
+    {'subL':'리바이탈', 'subLink':'beyond.html'},
+    {'subL':'워터풀', 'subLink':'beyond.html'},
+    {'subL':'키즈에코', 'subLink':'beyond.html'}
+    ]
+    ]},
 
-  {'title':{'name':'메이크업','link':'beyond.html','target':'_blank'},
-  'subContent':[
+  { 'title':{'name':'헤어','link':'beyond.html','target':'_blank'},
+    'sub':[
+  [
+   {'subC':'샴푸/린스', 'subLink':'beyond.html'},
+   {'subC':'에센스/트리트먼트/스케일링', 'subLink':'beyond.html'},
+   {'subC':'스타일링', 'subLink':'beyond.html'}
+  ],
+
+  [
+   {'subL':'프로페셔널 디펜스', 'subLink':'beyond.html'},
+   {'subL':'스캘프 클리닉', 'subLink':'beyond.html'},
+   {'subL':'딥 클린', 'subLink':'beyond.html'},
+   {'subL':'데미지 리페어', 'subLink':'beyond.html'},
+   {'subL':'키즈에코', 'subLink':'beyond.html'},
+   {'subL':'아르간 테라피', 'subLink':'beyond.html'},
+   {'subL':'스페셜', 'subLink':'beyond.html'},
+   {'subL':'스트레스 릴리프', 'subLink':'beyond.html'},
+   {'subL':'딥모이스처', 'subLink':'beyond.html'}
+  ]
+   ]},
+
+  { 'title':{'name':'메이크업','link':'beyond.html','target':'_blank'},
+    'sub':[
+  [
+   {'subC':'페이스', 'subLink':'beyond.html'},
+   {'subC':'아이', 'subLink':'beyond.html'},
+   {'subC':'립', 'subLink':'beyond.html'},
+   {'subC':'네일', 'subLink':'beyond.html'}
+  ],
+  [
+   {'subL':'엔젤아쿠아', 'subLink':'beyond.html'},
+   {'subL':'아쿠아 틴티드루즈', 'subLink':'beyond.html'},
+   {'subL':'타임리스', 'subLink':'beyond.html'},
+   {'subL':'피토 아쿠아', 'subLink':'beyond.html'},
+   {'subL':'리치 컬러 립스틱', 'subLink':'beyond.html'},
+   {'subL':'립 스무더', 'subLink':'beyond.html'},
+   {'subL':'퓨어 내츄럴 립밤', 'subLink':'beyond.html'},
+   {'subL':'기타 색조', 'subLink':'beyond.html'}
+  ]
+   ]},
+
+  { 'title':{'name':'온라인 전용','link':'beyond.html','target':'_blank'},
+    'sub':[
+  [
+   {'subC':'온라인 전용상품', 'subLink':'beyond.html'}
+  ],
+
+  [
   {'subL':'엔젤아쿠아', 'subLink':'beyond.html'},
   {'subL':'아쿠아 틴티드루즈', 'subLink':'beyond.html'},
   {'subL':'타임리스', 'subLink':'beyond.html'},
@@ -134,47 +147,28 @@ var lineCon = [
   {'subL':'리치 컬러 립스틱', 'subLink':'beyond.html'},
   {'subL':'립 스무더', 'subLink':'beyond.html'},
   {'subL':'퓨어 내츄럴 립밤', 'subLink':'beyond.html'},
-  {'subL':'기타 색조', 'subLink':'beyond.html'}]
- },
-
-  {'title':{'name':'프리미엄','link':'beyond.html','target':'_blank'},
-     'subContent':[
-     {'subL':'엔젤아쿠아', 'subLink':'beyond.html'},
-     {'subL':'아쿠아 틴티드루즈', 'subLink':'beyond.html'},
-     {'subL':'타임리스', 'subLink':'beyond.html'},
-     {'subL':'피토 아쿠아', 'subLink':'beyond.html'},
-     {'subL':'리치 컬러 립스틱', 'subLink':'beyond.html'},
-     {'subL':'립 스무더', 'subLink':'beyond.html'},
-     {'subL':'퓨어 내츄럴 립밤', 'subLink':'beyond.html'},
-     {'subL':'기타 색조', 'subLink':'beyond.html'}]
-    },
-
-  {'title':{'name':'선물추천','link':'beyond.html','target':'_blank'},
-  'subContent':[
-  {'subL':'BEST', 'subLink':'beyond.html'},
-  {'subL':'1~2만원대', 'subLink':'beyond.html'},
-  {'subL':'3~4만원대', 'subLink':'beyond.html'},
-  {'subL':'5만원대 이상', 'subLink':'beyond.html'}]
- }
+  {'subL':'기타 색조', 'subLink':'beyond.html'}
+  ]
+  ]},
 
 ];
 
 // code_list===============================================================
-var gnbContentList = '<div class="open_area"><ul class="tab"><li><a href="#">카테고리별</a></li><li><a href="#">라인별</a></li></ul><div class="category"><ul></ul></div><div class="line"><ul></ul></div></div>';
+var gnbContentList = '<div class="open_area"><div class="category"><ul></ul></div></div>';
 
 var openMenu = gnbBox.children('.open_menu');
 var openMenuUl = openMenu.children('ul');
 
-var categoryLen = categoryCon.length;
-var i, j, gnbLi, gnbEq, titleN, categoryWrap, tabUl, tabOn;
+var arrLen = arr.length;
+var i, j, gnbLi, titleN, categoryWrap, tabUl, tabOn;
 
-for(i=0; i<categoryLen ; i+=1){
+
+for(i=0; i<arrLen ; i+=1){
   openMenuUl.append('<li></li>');
   
   gnbLi = openMenuUl.children('li').eq(i);
-  gnbEq = gnbLi.children('a');
   
-  titleN = categoryCon[i].title;
+  titleN = arr[i].title;
   gnbLi.append('<a href="'+ titleN.link +'" target="'+ titleN.target +'">'+ titleN.name +'</a>');
 
   gnbLi.append(gnbContentList);
@@ -183,28 +177,18 @@ for(i=0; i<categoryLen ; i+=1){
   tabOn.addClass('on');
 
 // --------------------------------------------------------------------------------------------------
-subConLen = categoryCon[i].subContent.length;
-for(j=0; j<subConLen ; j+=1){
+var cateLen = arr[i].sub[0].length;
+for(j=0; j<cateLen ; j+=1){
 
   categoryWrap = gnbLi.find('.category');
   categoryUl = categoryWrap.children('ul');
   
-  subN = categoryCon[i].subContent[j];
-  categoryUl.append('<li><a href="'+subN.subLink+'">'+subN.subC+'</a></li>');
+  cateLenCon = arr[i].sub[0][j];
+  categoryUl.append('<li><a href="'+cateLenCon.subLink+'">'+cateLenCon.subC+'</a></li>');
   }
 
-  lineConLen = lineCon[i].subContent.length;
-  
-  // -----------------------------------------------------------------
-  for(j=0; j<lineConLen ; j+=1){
-    
-    lineWrap = gnbLi.find('.line');
-    lineUl = lineWrap.children('ul');
-    
-    subl = lineCon[i].subContent[j];
-    lineUl.append('<li><a href="'+subl.subLink+'">'+subl.subL+'</a></li>');
-  }
 }//for
+
 
 // 아코디언--------------------------------------------------
 var openMenuLi = openMenu.find('li');
@@ -246,26 +230,25 @@ var openLi = openMenu.find('li');
 var tab = openLi.find('.tab');
 var tabLi = tab.children('li');
 var tabLink = tabLi.children('a');
-var openAreaCon = openArea.children('div');
 
 tabLink.on('click',['a'],function(e){
   e.preventDefault();
 
   var it = $(this);
-  var liIndex = it.parent().index();
+  var liIndex = it.parents('.open_menu').find('li').index();
   var itI = it.index();
 
   it.addClass('on');
   it.siblings().removeClass('on');
 
-  
-  // openAreaCon.eq(itI).show();
-  // openAreaCon.eq(itI).siblings('div').hide();
-
-  // tabLi.eq(itI).addClass('on');
-  // tabLi.eq(itI).siblings().removeClass('on');
-
+  if(itI === 0){
+    it.parents('.open_menu').find('.category').show();
+  }else{
+    it.parents('.open_menu').find('.line').show();
+  }
 
 });
+
+
 
 })(jQuery);
